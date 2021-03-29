bb0df04423b06fe43d80e6f30fa465f411ac4549
Rahul Hingmire <67729354+rahulhingmire@users.noreply.github.com>
2021-03-29 12:09:15 +0530

    Create Pattern5.java

---
+++ Pattern5.java
@@ -0,0 +1,24 @@
+package NestedLoops;
+
+import java.util.Scanner;
+
+public class Pattern5 {
+
+	public static void main(String[] args) {
+		int number=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n=sc.nextInt();
+		for(int i=0;i<=n;i++) {
+			for(int j=1;j<(n-i+1);j++) {
+				System.out.print(" ");
+		}
+		for(int k=1;k<=i;k++) {
+			System.out.print(number++ +" ");
+		}
+		System.out.println();
+	}
+
+	}
+
+}
